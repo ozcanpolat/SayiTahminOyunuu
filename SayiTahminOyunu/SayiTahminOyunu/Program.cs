87283068b3d01c618afea3829d7ecc1520866608
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-08 21:50:33 +0300

    SayiTahminOyunu

---
+++ SayiTahminOyunu/SayiTahminOyunu/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SayiTahminOyunu
+{
+    internal class Program
+    {
+        static Random rastgele = new Random();
+
+        static void Main(string[] args)
+        {
+            int tahmin, sayi = rastgele.Next(100);
+            int hak = 5;
+
+            while (true)
+            {
+                Console.WriteLine("\n\n" + hak-- + " hakkınız var.");
+                if (hak < 0)
+                {
+                    Console.WriteLine("KAYBETTİNİZ!");
+                    break;
+                }
+
+                Console.Write("SAYI GİRİNİZ: ");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if (tahmin > sayi)
+                {
+                    Console.WriteLine("Daha KÜÇÜK bir sayı giriniz.");
+                }
+                else if (tahmin < sayi)
+                {
+                    Console.WriteLine("Daha BÜYÜK bir sayı giriniz.");
+                }
+                else
+                {
+                    Console.WriteLine("TEBRİKLER!");
+                }
+            }
+
+        }
+    }
+}
